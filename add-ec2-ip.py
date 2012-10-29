f91e27a6ed65b09b1fc819ade191d6d3ed23b2e0
Julien Graglia <jgraglia@netceler.com>
2012-10-30 00:14:00 +0100

    refactoring time...

--- add-ec2-ip.py
+++ add-ec2-ip.py
@@ -7,17 +7,50 @@ from boto.ec2.connection import EC2Connection
 from time import time
 from datetime import datetime
 
-def findIP():
-    '''
-    simple python script to return your public IP address using whatismyip.com
-    if executed from command line will display IP
+''' WARN : Boto seems to require Python 2.x '''
 
-    Returns your IP address. Includes setting the header to match the request
-    see http://www.whatismyip.com/faq/automation.asp
-    '''
-    headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0)' \
-                    + ' Gecko/20100101 Firefox/12.0' }
-    return urllib2.urlopen(urllib2.Request("http://automation.whatismyip.com/n09230945.asp",None, headers )).read()
+'''
+Public IP "providers" :
+    http://agentgatech.appspot.com/
+    http://automation.whatismyip.com/n09230945.asp
+'''
+
+class WhatIsMyIpRetriever():
+	def __str__( self ):
+		return "http://automation.whatismyip.com/n09230945.asp"
+
+	def retrievePublicIp(self): 
+	    '''
+	    Includes setting the header to match the request
+	    see http://www.whatismyip.com/faq/automation.asp
+	    '''
+	    headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0' }
+	    return urllib2.urlopen(urllib2.Request("http://automation.whatismyip.com/n09230945.asp", None, headers )).read()
+
+class AgentGatechIpRetriever():
+	def __str__( self ):
+		return "http://agentgatech.appspot.com/"
+	def retrievePublicIp(self): 
+	    headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Firefox/12.0' }
+	    return urllib2.urlopen(urllib2.Request("http://agentgatech.appspot.com/", None, headers)).read()
+
+class DryRuleActivator():
+	def __init__(self, securityGroup):
+		pass
+	def authorize(self, pub_ip_range, tcpPort):
+		print "DRY RUN >> AUTH   >> ",  pub_ip_range, ", TCP: ", tcpPort
+	def revoke(self, pub_ip_range, tcpPort):
+		print "DRY RUN >> REVOKE  >> ",  pub_ip_range, ", TCP: ", tcpPort
+
+class DefaultRuleActivator():
+	def __init__(self, securityGroup):
+		self.securityGroup=securityGroup
+	def authorize(self, pub_ip_range, tcpPort):
+		print "           AUTH   >> ",  pub_ip_range, ", TCP: ", tcpPort
+	    	self.securityGroup.authorize(ip_protocol='tcp', from_port=tcpPort, to_port=tcpPort, cidr_ip=pub_ip_range)
+	def revoke(self, pub_ip_range, tcpPort):
+		print "           REVOKE  >> ",  pub_ip_range, ", TCP: ", tcpPort
+	    	self.securityGroup.revoke('tcp', tcpPort, tcpPort, cidr_ip=pub_ip_range)
 
 def lookupSecurityGroupByName(conn, name):
     try:
@@ -31,9 +64,11 @@ def lookupSecurityGroupByName(conn, name):
 		print "   -> ", sg.name
 	sys.exit(1)
 
+#http://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing
 def createIpRange24(ip):
     return '%s0/24' % ip[:-len(ip.split('.')[-1])]
 
+#http://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing
 def createIpRange32(ip):
     return '%s/32' % ip
 
@@ -44,19 +79,24 @@ def addIPSecurity(group, ports=22, dryRun=False):
     print "Connected to", conn
     sg = lookupSecurityGroupByName(conn, group)
     print "Targeted Security Group: ", sg.name
-    pub_ip = findIP()
-    print "Public IP:", pub_ip
+    instances = sg.instances()
+    print len(instances) ,"instances will be affected"
+    for instance in instances:
+    	print "  -> EC2 Instance: ", instance.id, instance.public_dns_name, instance.state, instance.instance_type, " started at", instance.launch_time
+    publicIpRetriever = AgentGatechIpRetriever()
+    pub_ip = publicIpRetriever.retrievePublicIp()
+    print "Current public IP:", pub_ip, "(retrieved by ",publicIpRetriever,")"
     pub_ip_range = createIpRange32(pub_ip)
     print len(sg.rules), "rules actually defined in", sg.name
+    for rule in sg.rules:
+     	print rule
+    ruleActivator = DryRuleActivator(sg) if dryRun else DefaultRuleActivator(sg)
     for port in args.ports:
-	if dryRun:
-		print "DRY RUN >> AUTH   >> ",  pub_ip_range, ", TCP: ", port
-	else:
-	    	sg.authorize(ip_protocol='tcp', from_port=port, to_port=port, cidr_ip=pub_ip_range)
+	ruleActivator.authorize(pub_ip_range, port)
     print "Now, ", len(sg.rules), "rules defined in", sg.name
 
     print "========================================================="
-    print "=========== PRESS A KEY TO REMOVE RULES ================="
+    print "=========== PRESS A KEY TO REVOKE RULES ================="
     print "========================================================="
     authTime=datetime.now()
     print authTime
@@ -64,10 +104,7 @@ def addIPSecurity(group, ports=22, dryRun=False):
    
     print "Duration:",datetime.now()-authTime
     for port in args.ports:
-	if dryRun:
-		print "DRY RUN >> REVOKE  >> ",  pub_ip_range, ", TCP: ", port
-	else:
-	    	sg.revoke('tcp', port, port, cidr_ip=pub_ip_range)
+	ruleActivator.revoke(pub_ip_range, port)
     print "Now, ", len(sg.rules), "rules defined in", sg.name
 
 
